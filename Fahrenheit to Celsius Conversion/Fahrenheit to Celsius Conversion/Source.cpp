988ce057e023738f36392440b1ea800a6eec92af
Aaron Lang <blackthornlm@gmail.com>
2018-08-30 11:17:45 -0500

    Started F to C conversion program

---
+++ Fahrenheit to Celsius Conversion/Fahrenheit to Celsius Conversion/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double tempAt8;
+	double tempAt12;
+	double tempAt5;
+
+	cout << "Enter the temperature at 8:00 AM: ";
+	cin >> tempAt8;
+	
+	cout << "Enter the temperature at 12:00 PM: "; 
+	cin >> tempAt12;
+
+	cout << "Enter the temperature at 5:00 PM: ";
+	cin >> tempAt5;
+
+
+
+
+	system("pause");
+	return 0;
+}
