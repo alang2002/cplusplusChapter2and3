25a8849d3422ed23a57921f45e02c8b28d034c29
Aaron Lang <blackthornlm@gmail.com>
2018-09-04 10:32:06 -0500

    Lake View Hotel completed

--- LakeViewHotel/LakeViewHotel/Source.cpp
+++ LakeViewHotel/LakeViewHotel/Source.cpp
@@ -2,13 +2,29 @@
 #include <string> 
 using namespace std;
 
+double totalCharge(int nStayed, int mOnPhone)
+{
+	int perNightRate = 100;
+	double chargePerMin = 0.25;
+	double total = (nStayed * perNightRate) + (mOnPhone * chargePerMin);
+	return total;
+}
+
+
 int main()
 {
 	string name;
 	int nightsStayed;
-	int perNightRate = 100;
+	int minutesOnTelephone;
 
+	cout << "Please enter your name: ";
+	cin >> name;
+	cout << "How many nights did you stay? ";
+	cin >> nightsStayed;
+	cout << "How many minutes did you spend talking on the telephone? ";
+	cin >> minutesOnTelephone;
 
+	cout << name << ", your total is $" << totalCharge(nightsStayed, minutesOnTelephone) << "." << endl;
 
 	system("pause");
 	return 0;
